89c18fd2b553cdad9afa362e22baa5348bd619af
Abdullah Gül <gulabdullah162@gmail.com>
2025-04-27 22:42:05 +0300

    İlk commit: C programı dosyaları

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define pi
+
+int main () {
+float radius ;
+float height ;
+printf("please enter the value of radius: ");
+scanf("%f", &radius);
+printf("please enter the value of height: ");
+scanf("%f", &height);
+float volume ;
+volume= pi * r * r * h ;
+float surface area ;
+surface area = 2 * pi * r * h + 2 * pi * r * r ;
+printf("The value of volume is : " %f , &volume /n);
+printf("The value of surface area is : " %f, &surface area)
+
+return 0 ;
+}
